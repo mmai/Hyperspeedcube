bba767313c921980c413db2acff33bc046c08a75
Henri Bourcereau <henri.bourcereau@gmail.com>
2023-04-13 09:12:06 +0200

    nix devenv

---
+++ devenv.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [
+    # pour erreurs pendant compilation
+    pkgs.cmake
+    pkgs.fontconfig
+    pkgs.harfbuzz
+    pkgs.freetype
+    pkgs.expat
+    pkgs.glib
+    pkgs.atk 
+    pkgs.gtk3 
+    pkgs.pango
+    pkgs.cairo
+    pkgs.gdk-pixbuf 
+    pkgs.zlib
+    pkgs.xorg.libxcb
+
+    # pour erreur à l'exécution, selon https://github.com/emilk/egui/discussions/1587
+    pkgs.libxkbcommon
+    pkgs.libGL
+
+    # WINIT_UNIX_BACKEND=wayland
+    pkgs.wayland
+
+    # WINIT_UNIX_BACKEND=x11
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXi
+    pkgs.xorg.libX11
+
+    pkgs.vulkan-headers pkgs.vulkan-loader
+  ];
+
+  enterShell = ''
+  cargo run --release
+  '';
+
+  # https://devenv.sh/languages/
+  languages.rust.enable = true;
+
+}
